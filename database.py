0a1eaa8471519641d3791d3c69a75705377d7b7f
aboud20-meet <aboud20@meet.mit.edu>
2018-11-15 19:01:04 +0200

    ghjk

--- database.py
+++ database.py
@@ -9,9 +9,18 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 def create_cat(name):
-    cat_object = Cat(name=name)
+    cat_object = Cat(name=name, vote=0)
     session.add(cat_object)
     session.commit()
+
+def get_cat_id(id):
+	cat=session.query(Cat).filter_by(id=id).first()
+	return cat
+
+def add_vote(id):
+	cat=get_cat_id(id=id)
+	cat.vote=cat.vote+1
+	session.commit()
 
 def get_all_cats():
     cats = session.query(Cat).all()
